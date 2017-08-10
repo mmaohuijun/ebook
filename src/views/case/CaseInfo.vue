4df36f3c4904ec01ea51651b27bb003e21995f6c
lizhenlong <lizhenlong@tospur.com>
2017-08-10 16:49:21 +0800

    updated css

--- src/views/case/CaseInfo.vue
+++ src/views/case/CaseInfo.vue
@@ -1,90 +1,3 @@
-<style lang="less">
-  .field-box {
-    border:1px solid #979797;
-    background: #fff;
-    padding: 16px 33px;
-  }
-  .field-logo {
-    float: left;
-    width: 156px;
-    height: 156px;
-    background: #eee;
-    border-radius: 4px;
-    text-align: center;
-
-    img {
-      display: block;
-      width: 156px;
-      height: 156px;
-      border-radius: 4px;
-      margin-bottom: 20px;
-    }
-  }
-
-  .field-bottom-btn {
-    text-align: right;
-    padding: 30px 24px 0 0;
-  }
-
-  .field-bg {
-    position: relative;
-    float: right;
-    background: #eee;
-
-    img {
-      display: block;
-      width:450px;
-      height:228px;
-    }
-
-    .btn-upload-field-bg {
-      position: absolute;
-      top: 16px;
-      right: 19px;
-      opacity:0.3;
-      background:#000000;
-      border-radius:4px;
-      width:100px;
-      height:32px;
-      text-align: center;
-      font-size:18px;
-      color:#ffffff;
-      border: none;
-      padding: 0;
-
-      span {
-        line-height:32px;
-      }
-    }
-  }
-
-  .field-map {
-    width: 100%;
-    height: 430px;
-  }
-
-  .custom-tabs {
-    &.ivu-tabs.ivu-tabs-card>.ivu-tabs-bar .ivu-tabs-nav-container {
-      height: 50px;
-    }
-    &.ivu-tabs.ivu-tabs-card>.ivu-tabs-bar .ivu-tabs-tab {
-      font-size: 20px !important;
-      height: 49px;
-      line-height: 50px;
-      padding: 0 28px;
-      background-color: #fff;
-      border-color: #fff;
-    }
-
-    &.ivu-tabs.ivu-tabs-card>.ivu-tabs-bar .ivu-tabs-tab-active {
-      font-size: 20px !important;
-      height: 50px;
-      line-height: 50px;
-      border-color: #ddd;
-      color: #111;
-    }
-  }
-</style>
 <template>
 <div class="layout__content">
   <div class="layout__header">
@@ -558,3 +471,90 @@ export default {
   }
 }
 </script>
+<style lang="less">
+  .field-box {
+    border:1px solid #979797;
+    background: #fff;
+    padding: 16px 33px;
+  }
+  .field-logo {
+    float: left;
+    width: 156px;
+    height: 156px;
+    background: #eee;
+    border-radius: 4px;
+    text-align: center;
+
+    img {
+      display: block;
+      width: 156px;
+      height: 156px;
+      border-radius: 4px;
+      margin-bottom: 20px;
+    }
+  }
+
+  .field-bottom-btn {
+    text-align: right;
+    padding: 30px 24px 0 0;
+  }
+
+  .field-bg {
+    position: relative;
+    float: right;
+    background: #eee;
+
+    img {
+      display: block;
+      width:450px;
+      height:228px;
+    }
+
+    .btn-upload-field-bg {
+      position: absolute;
+      top: 16px;
+      right: 19px;
+      opacity:0.3;
+      background:#000000;
+      border-radius:4px;
+      width:100px;
+      height:32px;
+      text-align: center;
+      font-size:18px;
+      color:#ffffff;
+      border: none;
+      padding: 0;
+
+      span {
+        line-height:32px;
+      }
+    }
+  }
+
+  .field-map {
+    width: 100%;
+    height: 430px;
+  }
+
+  .custom-tabs {
+    &.ivu-tabs.ivu-tabs-card>.ivu-tabs-bar .ivu-tabs-nav-container {
+      height: 50px;
+    }
+    &.ivu-tabs.ivu-tabs-card>.ivu-tabs-bar .ivu-tabs-tab {
+      font-size: 20px !important;
+      height: 49px;
+      line-height: 50px;
+      padding: 0 28px;
+      background-color: #fff;
+      border-color: #fff;
+    }
+
+    &.ivu-tabs.ivu-tabs-card>.ivu-tabs-bar .ivu-tabs-tab-active {
+      font-size: 20px !important;
+      height: 50px;
+      line-height: 50px;
+      border-color: #ddd;
+      color: #111;
+    }
+  }
+</style>
