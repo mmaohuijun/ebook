04f767d4f758bf55a6a6faefdcd409a692795b30
raoqingbiao <raoqingbiao@tospur.com>
2017-08-29 16:01:43 +0800

    modify caseproject.vue

--- src/views/case/CaseProject.vue
+++ src/views/case/CaseProject.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="case-project">
-  <a href="javascript:;" class="case-add-btn" @click="modal('新建项目')">新建项目</a>
+  <a href="javascript:;" class="case-add-btn" @click="addProject">新建项目</a>
   <Row :gutter="24" class="case-project__list">
     <Col :md="12" v-for="(items, index) in caseList" :key="index">
       <div class="case-project__item">
@@ -35,7 +35,7 @@
             <Input style="width: 180px;" v-model="inputModel.name"></Input>
           </Form-item>
           <Form-item label="物业选择：">
-            <Select style="width: 180px;" @on-change="changeProperty" v-model="inputModel.propertyTypeId">
+            <Select style="width: 180px;" @on-change="changeProperty" v-model="inputModel.propertyType">
               <Option value="1">别墅</Option>
               <Option value="2">住宅</Option>
               <Option value="3">酒店式公寓</Option>
@@ -52,18 +52,18 @@
             <span style="font-size: 16px; vertical-align: middle;">厅</span>
           </Form-item>
           <Form-item>
-             <!-- <Select placeholder="" style="width: 74px;" v-model="inputModel.apartTypes3" @on-change="apartTypes3Change">
-              <Option v-for="n in (propertyNumber / propertyGutter)" :value="(n * propertyGutter)">{{n * propertyGutter}}</Option>
-            </Select>  -->
-            <Select placeholder="" style="width: 74px;" v-show="inputModel.propertyTypeId === '1'" v-model="inputModel.apartTypes3" @on-change="apartTypes3Change">
+            <Select placeholder="" style="width: 74px;" v-show="inputModel.propertyType === '1'" v-model="inputModel.apartTypes3">
               <Option v-for="n in (1000 / 50)" :value="(n * 50)" :key="n">{{n * 50}}</Option>
             </Select>
-            <Select placeholder="" style="width: 74px;" v-show="inputModel.propertyTypeId !== '1'" v-model="inputModel.apartTypes3">
+            <Select placeholder="" style="width: 74px;" v-show="inputModel.propertyType !== '1'" v-model="inputModel.apartTypes3">
               <Option v-for="n in (300 / 10)" :value="(n * 10)" :key="n">{{n * 10}}</Option>
-            </Select>  
+            </Select>
             <span style="margin: 0 10px; font-size: 16px; vertical-align: middle;">-</span>
-            <Select placeholder=""  style="width: 74px;" v-model="inputModel.apartTypes4">
-              <Option v-for="n in (propertyNumber / propertyGutter)" :value="(n * propertyGutter)" :key="n">{{n * propertyGutter}}</Option>
+            <Select placeholder="" style="width: 74px;" v-show="inputModel.propertyType === '1'" v-model="inputModel.apartTypes4">
+              <Option v-for="n in (1000 / 50)" :value="(n * 50)" :key="n">{{n * 50}}</Option>
+            </Select>
+            <Select placeholder="" style="width: 74px;" v-show="inputModel.propertyType !== '1'" v-model="inputModel.apartTypes4">
+              <Option v-for="n in (300 / 10)" :value="(n * 10)" :key="n">{{n * 10}}</Option>
             </Select>
             <span style="font-size: 16px; vertical-align: middle;">㎡</span>
             <i class="iconfont icon-tianjia btn-add-property" @click="addProperty"></i>
@@ -72,30 +72,16 @@
       </Col>
       <Col :md="12" style="padding: 28px 0 0 17px;">
         <div class="case-list">
-          <div class="case-item">
-            <span class="case-item__label">别墅：</span>
-            <p class="case-item__content">三室两厅100-200㎡<i class="iconfont icon-shanchu"></i></p>
-            <p class="case-item__content">三室两厅100-200㎡<i class="iconfont icon-shanchu"></i></p>
-            <p class="case-item__content">三室两厅100-200㎡<i class="iconfont icon-shanchu"></i></p>
-          </div>
-          <div class="case-item">
-            <span class="case-item__label">住宅：</span>
-            <p class="case-item__content">三室两厅100-200㎡<i class="iconfont icon-shanchu"></i></p>
-            <p class="case-item__content">三室两厅100-200㎡<i class="iconfont icon-shanchu"></i></p>
-            <p class="case-item__content">三室两厅100-200㎡<i class="iconfont icon-shanchu"></i></p>
-          </div>
-          <div class="case-item">
-            <span class="case-item__label">酒店式公寓：</span>
-            <p class="case-item__content">三室两厅100-200㎡<i class="iconfont icon-shanchu"></i></p>
-            <p class="case-item__content">三室两厅100-200㎡<i class="iconfont icon-shanchu"></i></p>
-            <p class="case-item__content">三室两厅100-200㎡<i class="iconfont icon-shanchu"></i></p>
+          <div class="case-item" v-for="(items, propertyKey) in caseProject.propertys">
+            <span class="case-item__label">{{items.propertyTypeName}}：</span>
+            <p class="case-item__content" v-for="(item, itemKey) in items.apartTypes">{{item}}<i class="iconfont icon-shanchu" @click="deleteProperty(propertyKey, itemKey)"></i></p>
           </div>
         </div>
       </Col>
     </Row>
     <div slot="footer">
       <Button type="text" size="large" @click="modalShow = false">取消</Button>
-      <Button type="primary" size="large">完成</Button>
+      <Button type="primary" size="large" @click="saveProject">完成</Button>
     </div>
   </Modal>
 </div>
@@ -168,32 +154,30 @@ export default {
         name: '中南一期1',
         propertys: [
           {
+            propertyType: '1',
             propertyTypeName: '别墅',
-            propertyTypeId: '1',
-            apartTypes: ['1房1厅 50-70㎡', '1房1厅 70-90㎡']
+            apartTypes: ['1房1厅 50-70㎡', '2房1厅 70-90㎡']
           },
           {
+            propertyType: '2',
             propertyTypeName: '住宅',
-            propertyTypeId: '2',
-            apartTypes: ['1房1厅 50-70㎡', '1房1厅 70-90㎡']
+            apartTypes: ['4房1厅 50-70㎡', '4房1厅 70-90㎡']
           },
           {
+            propertyType: '3',
             propertyTypeName: '酒店式公寓',
-            propertyTypeId: '3',
-            apartTypes: ['1房1厅 50-70㎡', '1房1厅 70-90㎡']
+            apartTypes: ['5房1厅 50-70㎡', '6房1厅 70-90㎡']
           }
         ]
       },
       inputModel: {
         name: '',
-        propertyTypeId: '',
+        propertyType: '',
         apartTypes1: '',
         apartTypes2: '',
         apartTypes3: 0,
         apartTypes4: 0
       }
-      // propertyNumber: 1000,
-      // propertyGutter: 50
     }
   },
   computed: {
@@ -201,34 +185,21 @@ export default {
       return this.$store.state.CASE_ID
     },
     propertyNumber() {
-      return this.inputModel.propertyTypeId === '1' ? 1000 : 300
+      return this.inputModel.propertyType === '1' ? 1000 : 300
     },
     propertyGutter() {
-      return this.inputModel.propertyTypeId === '1' ? 50 : 10
+      return this.inputModel.propertyType === '1' ? 50 : 10
     }
   },
   methods: {
-    apartTypes3Change(v1) {
-      console.log('apartTypes3Change', v1, this.inputModel.propertyTypeId)
-    },
-    apartTypes4Change(v1) {
-      console.log('apartTypes4Change', v1)
-    },
     getList() {
       this.$axios.post('/case-project/list', { caseId: this.caseId }).then(response => {
         if (response === null) return
         this.caseList = response.data
+        console.log(this.caseList)
       })
     },
     changeProperty(value) {
-      console.log('changeProperty', this.propertyNumber, this.propertyGutter, value)
-      // if (value === '1') {
-      //   this.propertyNumber = 1000
-      //   this.propertyGutter = 50
-      // } else {
-      //   this.propertyNumber = 300
-      //   this.propertyGutter = 10
-      // }
       this.inputModel.apartTypes3 = ''
       this.inputModel.apartTypes4 = ''
     },
@@ -236,17 +207,38 @@ export default {
       this.modalTitle = title
       this.modalShow = true
     },
+    addProject() {
+      this.caseProject.id = '0'
+      this.caseProject.name = ''
+      this.caseProject.propertys = []
+      this.modal('新建项目')
+    },
     modifyCase(item) {
       console.log(item)
-      this.case = item
+      this.caseProject = item
+    },
+    deleteCase(index) {
+      console.log(index)
     },
     addProperty() {
       console.log(this.inputModel)
+    },
+    deleteProperty(key, index) {
+      this.caseProject.propertys[key].apartTypes.splice(index, 1)
+    },
+    saveProject() {
+      const data = {
+        id: this.caseProject.id,
+        caseId: this.caseId,
+        name: this.caseProject.name,
+        propertys[i].propertyType:
+      }
+      this.$axios.post('/case-project/save', { params: data }).then(response => {
+        if (response === null) return
+      })
     }
-
   },
   mounted() {
-    console.log('caseId:' + this.caseId)
     this.getList()
   }
 }
@@ -332,6 +324,7 @@ export default {
 .case-item {
   line-height: 27px;
   margin-bottom: 15px;
+  overflow: hidden;
 }
 
 .case-item__label {
