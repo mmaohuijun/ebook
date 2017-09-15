5104e68bb1e42e6575d730cc85604cf1faa37394
lizhenlong <lizhenlong@tospur.com>
2017-09-15 11:42:35 +0800

    updated auth

--- src/views/authority/Authority.vue
+++ src/views/authority/Authority.vue
@@ -8,36 +8,20 @@
   </div>
   <div class="layout__body">
     <table class="authority" cellspacing="0" cellpadding="0" border="0">
-      <tr>
-        <td class="authority-row-title">权限一</td>
-        <td>
-          <div class="authority-check-all"><Checkbox>案场</Checkbox></div>
-        </td>
-        <td>
-          <div class="authority-check-all"><Checkbox>组织</Checkbox></div>
-        </td>
-        <td>
-          <div class="authority-check-all"><Checkbox>用户</Checkbox></div>
-          <div class="authority-check-items">
-            <div class="authority-check-item"><Checkbox>内部用户</Checkbox></div>
-            <div class="authority-check-item"><Checkbox>外部用户</Checkbox></div>
-            <div class="authority-check-item"><Checkbox>禁用用户</Checkbox></div>
+      <tr class="authority-row-current" v-for="(ele, index) in authData" :key="index"> 
+        <td class="authority-row-title">{{ele.title}}</td>
+        <td v-for="(item, indexNum) in ele.children" :key="indexNum">
+          <div class="authority-check-all">
+            <Checkbox :indeterminate="item.indeterminate" :value="item.checked">{{item.label}}</Checkbox>
           </div>
-        </td>
-        <td>
-          <div class="authority-check-all"><Checkbox>权限</Checkbox></div>
-        </td>
-        <td>
-          <div class="authority-check-all"><Checkbox>客户数据</Checkbox></div>
-          <div class="authority-check-items">
-            <div class="authority-check-item"><Checkbox>来电客户</Checkbox></div>
-            <div class="authority-check-item"><Checkbox>到访客户</Checkbox></div>
-            <div class="authority-check-item"><Checkbox>成交客户</Checkbox></div>
-            <div class="authority-check-item"><Checkbox>未分配客户</Checkbox></div>
-            <div class="authority-check-item"><Checkbox>新建客户</Checkbox></div>
+          <div class="authority-check-items" v-if="item.children.length !== 0">
+            <div class="authority-check-item" v-for="(subItem, subIndex) in item.children" :key="subIndex">
+              <Checkbox :value="subItem.checked">{{subItem.label}}</Checkbox>
+            </div>
           </div>
         </td>
       </tr>
+
       <tr class="authority-row-current">
         <td class="authority-row-title">权限一</td>
         <td>
@@ -47,7 +31,7 @@
           <div class="authority-check-all"><Checkbox>组织</Checkbox></div>
         </td>
         <td>
-          <div class="authority-check-all"><Checkbox :indeterminate="true">内部用户</Checkbox></div>
+          <div class="authority-check-all"><Checkbox :indeterminate="true" :value="true">用户</Checkbox></div>
           <div class="authority-check-items">
             <div class="authority-check-item"><Checkbox>内部用户</Checkbox></div>
             <div class="authority-check-item"><Checkbox>外部用户</Checkbox></div>
@@ -167,6 +151,51 @@ export default {
   name: 'Authority',
   data() {
     return {
+      authData: [
+        {
+          title: '权限1',
+          id: '100',
+          children: [
+            {
+              label: '案场',
+              key: '01',
+              indeterminate: false, // 是否有选中的子选项
+              checked: true,
+              children: []
+            },
+            {
+              label: '组织',
+              key: '02',
+              indeterminate: false, // 是否有选中的子选项
+              checked: false,
+              children: []
+            },
+            {
+              label: '用户',
+              key: '03',
+              indeterminate: true, // 是否有选中的子选项
+              checked: false,
+              children: [
+                {
+                  label: '内部用户',
+                  key: '04',
+                  checked: true
+                },
+                {
+                  label: '外部用户',
+                  key: '05',
+                  checked: false
+                },
+                {
+                  label: '禁用用户',
+                  key: '06',
+                  checked: false
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }
   }
 }
