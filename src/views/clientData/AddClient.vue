5e246010e4d83791d1dca5a2cc13d86cb0b40345
maohuijun <abc@DELL-PC.tospur.com>
2017-09-29 16:05:56 +0800

    update addClient

--- src/views/clientData/AddClient.vue
+++ src/views/clientData/AddClient.vue
@@ -6,25 +6,25 @@
   <div class="layout__body">
    <div class="addClient">
     <h1>新建客户</h1>
-     <Form ref="addForm" :model="addForm" :label-width="120" style="width:350px;margin: 0 auto">
-       <Form-item label="性别：">
+     <Form ref="addForm" :model="addForm" :rules="addForm" :label-width="120" style="width:350px;margin: 0 auto">
+       <Form-item label="性别：" prop="gender">
          <Radio-group v-model="addForm.gender">
            <Radio label="2">女</Radio>
            <Radio label="1">男</Radio>
          </Radio-group>
        </Form-item>
-       <Form-item label="姓名：">
+       <Form-item label="姓名：" prop="name">
            <Input v-model="addForm.name"></Input>
          </Form-item>
-         <Form-item label="电话号码：">
+         <Form-item label="电话号码：" prop="mobile">
            <Input v-model="addForm.mobile"></Input>
          </Form-item>
-         <Form-item label="案场：" v-if="caseShow">
+         <Form-item label="案场：" v-if="caseShow" prop="case">
            <Select v-model="addForm.case" @on-change="caseChange">
              <Option v-for="item in caseList" :value="item.id" :key="item.id">{{item.name}}</Option>
            </Select>
          </Form-item>
-         <Form-item label="顾问：" v-if="conShow">
+         <Form-item label="顾问：" v-if="conShow" prop="consultant">
            <Select v-model="addForm.consultant" @on-change="consultChange">
              <Option v-for="item in extUserList" :value="item.id" :key="item.id">{{item.name}}</Option>
            </Select>
@@ -42,6 +42,31 @@
 export default {
   name: 'AddClient',
   data() {
+    // const vgender = (rule, value, callback) => {
+    //   if (value === '') {
+    //     return callback(new Error('性别不能为空'))
+    //   }
+    // }
+    // const vname = (rule, value, callback) => {
+    //   if (value === '') {
+    //     return callback(new Error('姓名不能为空'))
+    //   }
+    // }
+    // const vmobile = (rule, value, callback) => {
+    //   if (value === '') {
+    //     return callback(new Error('电话号码不能为空'))
+    //   }
+    // }
+    // const case = (rule, value, callback) => {
+    //   if (value === '') {
+    //     return callback(new Error('案场不能为空'))
+    //   }
+    // }
+    // const vconsultant = (rule, value, callback) => {
+    //   if (value === '') {
+    //     return callback(new Error('顾问不能为空'))
+    //   }
+    // }
     return {
       addForm: {
         gender: '',
@@ -50,28 +75,45 @@ export default {
         consultant: '',
         case: ''
       },
+      // ruleForm: {
+      //   gender: [
+      //     { validator: vgender, trigger: 'blur' }
+      //   ],
+      //   name: [
+      //     { validator: vname, trigger: 'blur' }
+      //   ]
+      //   mobile: [
+      //     { validator: vmobile, trigger: 'blur' }
+      //   ],
+      //   case: [
+      //     { validator: vcase, trigger: 'blur' }
+      //   ],
+      //   consultant: [
+      //     { validator: vconsultn, trigger: 'blur' }
+      //   ],
+      // }
       pageNo: 1,
       pageSize: 20,
       extUserList: [],
       caseList: [],
       conShow: false,
       caseShow: false,
-      consultantId: '', // 被选中的顾问id
+      consultant: '', // 被选中的顾问id
       caseId: '' // 被选中的案场id
     }
   },
   methods: {
     // 获取顾问列表： 工号+姓名
-    getExtUserList() {
-      // this.addData = {
-      //   gender: this.addForm.gender
-      //   name: this.addForm.name
-      //   mobile: this.addForm.mobile
-      // }
-      this.$axios.post('/ext-user/worker-list', this.caseId).then(response => {
-        if (response === null) return
-      })
-    },
+    // getExtUserList() {
+    //   // this.addData = {
+    //   //   gender: this.addForm.gender
+    //   //   name: this.addForm.name
+    //   //   mobile: this.addForm.mobile
+    //   // }
+    //   this.$axios.post('/ext-user/worker-list', this.caseId).then(response => {
+    //     if (response === null) return
+    //   })
+    // },
     // 获取案场列表： 案场id+案场名
     getCaseList() {
       this.$axios.post('case/shortlist').then(response => {
@@ -84,9 +126,9 @@ export default {
       })
     },
     // 选择置业顾问
-    consultChange(consultantId) {
-      this.consultantId = consultantId
-      console.log('consultantid', this.consultantId)
+    consultChange(consultant) {
+      this.consultant = consultant
+      console.log('consultant', this.consultant)
     },
     // 选择案场
     caseChange(caseId) {
@@ -103,8 +145,8 @@ export default {
     // 判断身份 控制显示
     judgeIdentity() {
       console.log('judge', this.$store.state.user)
-      if (this.$store.state.user.type === 'case-worker') {
-        if (this.$store.state.user.adminFlag === true) {
+      if (this.getLoginInfo === 'case-worker') {
+        if (this.userAdminFlag === true) {
           // 外部用户 管理员权限
           this.conShow = true
           this.caseShow = false
@@ -131,26 +173,27 @@ export default {
       // 判断是否都不为空
         // 传值
       const data = {
-        gender: this.addForm.gender,
         name: this.addForm.name,
+        gender: this.addForm.gender,
         mobile: this.addForm.mobile
       }
       if (this.conShow === true && this.caseShow === false) {
-        data.consultantId = this.consultanttantId
+        data.consultant = this.consultant
       } else if (this.conShow === true && this.caseShow === true) {
-        data.consultantId = this.consultantId
+        data.consultant = this.consultant
         data.caseId = this.caseId
       }
       console.log('result data', data)
       this.$axios.post('case-cust/add', data).then(response => {
         if (response === null) return
         this.$Message.success('提交成功！')
-        this.resetFields('addForm')
+        // this.resetFields('addForm')
       })
     },
     // 重置表单
-    resetFileds(name) {
-      this.$refs[name].resetFileds()
+    resetFields(name) {
+      console.log('resetFields')
+      this.$refs[name].resetFields()
     },
     // 取消
     cancel() {
@@ -160,13 +203,16 @@ export default {
   },
   computed: {
     getLoginInfo() {
-      return this.$store.state.user
+      return this.$store.state.user.type
+    },
+    userAdminFlag() {
+      return this.$store.getters.adminFlag
     }
   },
   mounted() {
-    this.getExtUserList()
+    // this.getExtUserList()
     this.getCaseList()
-    console.log(this.getLoginInfo)
+    console.log(this.getLoginInfo, this.userAdminFlag)
     this.judgeIdentity()
   }
 }
