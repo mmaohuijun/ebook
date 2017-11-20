e6942293ff04c4e0635c3063830f7a80c175f334
maohuijun <abc@DELL-PC.tospur.com>
2017-11-20 17:08:11 +0800

    add case-searching

--- src/views/user/ExtUser.vue
+++ src/views/user/ExtUser.vue
@@ -112,11 +112,14 @@
     header-title="用户 - 外部用户"
     :dateSearch="true"
     :textSearch="true"
+    :caseSearch="isCaseSearch"
+    :caseList="caseList"
     placeholder="姓名／手机号"
     :uploadBtn="true"
     :addBtn="true"
     :lockBtn="lockBtn"
     :keyBtn="unlockBtn"
+    @onCaseChange="selectCase"
     @onDateSearch="dateSearch"
     @onTextSearch="textSearch"
     @onUpload="showUploadModal"
@@ -254,6 +257,8 @@ export default {
       }
     }
     return {
+      isCaseSearch: true,
+      caseList: [],
       confirmModal: {
         show: false,
         title: ''
@@ -285,7 +290,6 @@ export default {
       pageNo: 1,        // 页码
       total: 20,        // 数据总条数
       pageSize: 20,     // 每页条数
-      caseList: [],     // 案场列表
       selection: [],    // 选中的用户列表
       userInfo: {
         id: '',           // id
@@ -458,11 +462,14 @@ export default {
   mounted() {
     this.initUserList()
     this.getAuthList()
+    this.getCaseList()
+    this.justifyUser()
   },
   methods: {
     // 获取并渲染用户列表
     initUserList() {
       const data = {
+        caseName: this.caseName,
         name: this.name,
         startDate: this.startDate,
         endDate: this.endDate,
@@ -762,10 +769,27 @@ export default {
       this.pageNo = 1
       this.initUserList()
     },
+    // 案场搜索
+    selectCase(value) {
+      console.log('selectCase', value)
+      this.caseName = value
+      this.pageNo = 1
+      this.initUserList()
+    },
     // 改变分页
     pageChange(currentPage) {
       this.pageNo = currentPage
       this.initUserList()
+    },
+    // 判断是否为外部用户
+    justifyUser() {
+      if (this.ifCaseWorker) {
+        console.log('外部用户')
+        this.isCaseSearch = false
+      } else {
+        console.log('内部用户')
+        this.isCaseSearch = true
+      }
     }
   },
   components: {
